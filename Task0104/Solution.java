8b3a436b379447ff0febe4f9a240b98162e10d5f
Alexander Smolin <alexjavacore@mail.ru>
2022-10-02 14:00:42 +0300

    Task0104
    Конвертер строки

---
+++ Task0104/Solution.java
@@ -0,0 +1,15 @@
+
+/*
+Конвертер строки
+В методе main конвертируй строку string в тип double с помощью статического метода parseDouble класса Double.
+Затем округли полученное значение, используя статический метод round класса Math.
+Результат выведи на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        System.out.println(Math.round(Double.parseDouble(string)));
+    }
+}
+
