a885567a12c0e94d764fb5ce7e36571c78f7789a
Alexander Smolin <alexjavacore@mail.ru>
2022-10-06 00:00:20 +0300

    Task0113
    Следим за скобками

---
+++ Tasks01/Task0113/Solution.java
@@ -0,0 +1,13 @@
+/*
+Следим за скобками
+Кажется, у нас есть лишняя скобка? Или одной не хватает?
+Разберись-ка с этой проблемой.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
