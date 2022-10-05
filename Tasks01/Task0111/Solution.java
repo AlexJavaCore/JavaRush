2eca6a301a42ada3377b0726ad9e93e025fc4942
Alexander Smolin <alexjavacore@mail.ru>
2022-10-05 23:54:52 +0300

    Add files via upload

---
+++ Tasks01/Task0111/Solution.java
@@ -0,0 +1,12 @@
+/*
+Ох уж эти кавычки
+В коде уже есть кавычки, но, как оказалось, их недостаточно.
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
