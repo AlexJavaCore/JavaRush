a97058f4fde4a25ae4691dfc53555173b94f1675
Alexander Smolin <alexjavacore@mail.ru>
2022-10-05 23:58:04 +0300

    Task0112
    Кон, котэ и нация

---
+++ Tasks01/Task0112/Solution.java
@@ -0,0 +1,14 @@
+/*
+Кон, котэ и нация
+"Кон", "котэ" и "нация". Как соединить эти три строки?
+Ты же помнишь, как соединять String'и в Java?
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
