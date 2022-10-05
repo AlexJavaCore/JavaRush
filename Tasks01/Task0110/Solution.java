5f2ab64d12866a3b42d9af0978015ea365b0fc7b
Alexander Smolin <alexjavacore@mail.ru>
2022-10-05 23:52:33 +0300

    Task0110
    Точка и запятая

---
+++ Tasks01/Task0110/Solution.java
@@ -0,0 +1,12 @@
+/*
+Точка и запятая
+Точки и запятые важны не только в пунктуации в обычном тексте.
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
