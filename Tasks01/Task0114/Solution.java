4031c13fb939eaec846602e334eafe5eb8c7bd93
Alexander Smolin <alexjavacore@mail.ru>
2022-10-06 09:51:43 +0300

    Task0114
    И снова скобки

---
+++ Tasks01/Task0114/Solution.java
@@ -0,0 +1,16 @@
+/*
+И снова скобки
+Кажется, в этот раз проблема снова со скобками. Но уже с другими.
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
