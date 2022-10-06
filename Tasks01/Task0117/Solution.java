1ba5df82c26f20acbbdddf48bfe8eca035be2d9f
Alexander Smolin <alexjavacore@mail.ru>
2022-10-06 15:28:37 +0300

    Task0117
    Пакет нужен?

---
+++ Tasks01/Task0117/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1117;
+/*
+Пакет нужен?
+Исправь ошибку в коде, чтобы программа компилировалась и работала.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Test package");
+    }
+}
