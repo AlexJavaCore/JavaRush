7019db737011bc60a782a3ae901caa04eed38445
Alexander Smolin <alexjavacore@mail.ru>
2022-10-06 10:04:58 +0300

    Task0115
    Многострадальный метод main

---
+++ Tasks01/Task0115/Solution.java
@@ -0,0 +1,10 @@
+/*
+Многострадальный метод main
+Сделай одно исправление в коде, чтобы программа запускалась.
+*/
+
+public class Solution {
+    public static void main(String... errors) {
+        System.out.println("It works!");
+    }
+}
