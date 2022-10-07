7307170e0a852fd30922581c94d0488c21513290
Alexander Smolin <alexjavacore@mail.ru>
2022-10-07 23:48:26 +0300

    Task0111
    Ох уж эти кавычки

--- Tasks01/Task0111/Solution.java
+++ Tasks01/Task0111/Solution.java
@@ -3,7 +3,6 @@
 В коде уже есть кавычки, но, как оказалось, их недостаточно.
 Исправь ошибки в коде, чтобы программа компилировалась и работала.
 */
-
 public class Solution {
     public static void main(String[] args) {
         String string = "Amigo";
