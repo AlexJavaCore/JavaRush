4f7eb60d80acef5fa5772b7ca00bf52e7d194b87
Alexander Smolin <alexjavacore@mail.ru>
2022-10-07 07:30:22 +0300

    Task0118
    Импортный код

---
+++ Tasks01/Task0118/Solution.java
@@ -0,0 +1,15 @@
+/*
+Импортный код
+Исправь ошибку в коде, чтобы программа компилировалась и работала.
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
