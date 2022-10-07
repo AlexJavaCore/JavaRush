b44e7a44d1a7e2c8fa6c9b07106572ce4ea59d04
Alexander Smolin <alexjavacore@mail.ru>
2022-10-07 07:46:44 +0300

    Task0120
    Все буквы маленькие

---
+++ Tasks01/Task0119/Task0120/Solution.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+/*
+Все буквы маленькие
+Программа должна считать с консоли строку и вывести ее на экран в нижнем регистре.
+Исправь ошибку в коде, чтобы программа работала корректно.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        System.out.println(line.toLowerCase());
+    }
+}
