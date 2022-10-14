c277b411f6484c73c4b3fdaa8980d7599f6da474
Alexander Smolin <alexjavacore@mail.ru>
2022-10-14 10:40:43 +0300

    Task0218
    Shine bright like a diamond

---
+++ Tasks02/Task0218/Solution.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.*;
+
+/*
+Shine bright like a diamond
+The main method creates objects of different types.
+In some cases, you can omit the type of the object's
+elements and simply use the diamond(<>) operator.
+Change the element type of the object to the diamond operator
+where possible without changing the left side of the expressions.
+The order of the variables must not change.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        Stack<String> stack = new Stack<String>();
+        Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
