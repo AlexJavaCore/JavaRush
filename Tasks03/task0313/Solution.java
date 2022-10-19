62fb4ac529bfeca0e29e28b5cf665cc397b40689
Alexander Smolin <alexjavacore@mail.ru>
2022-10-19 09:17:17 +0300

    Task0313
    Успеваемость студентов-3

--- Tasks03/task0313/Solution.java
+++ Tasks03/task0313/Solution.java
@@ -3,7 +3,6 @@ package com.javarush.task.pro.task13.task1311;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /* 
 Успеваемость студентов-3
 В классе Solution объявлено поле grades типа HashMap<String, Double>,
