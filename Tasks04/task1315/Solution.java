f8d7a6a6659cc61e72ad60cf7c1b4e206357975f
Alexander Smolin <alexjavacore@mail.ru>
2022-10-22 22:29:02 +0300

    Add files via upload

---
+++ Tasks04/task1315/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+Нужно создать набор значений (enum) для времен года.
+Для этого нужно в отдельном файле создать enum Season и объявить в нем 4 значения.
+В методе Solution выведи все перечисления с новой строки.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Season.WINTER);
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+    }
+}
