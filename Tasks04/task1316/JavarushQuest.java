24786ffdcd797dd2de644b91cb2271da9c35cf99
Alexander Smolin <alexjavacore@mail.ru>
2022-10-23 15:48:28 +0300

    Add files via upload

---
+++ Tasks04/task1316/JavarushQuest.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task13.task1316;
+
+/* 
+Перечислим квесты Javarush
+В enum JavarushQuest хранится список квестов.
+Твоя задача — получить массив всех элементов в методе
+main (используй метод values()) и вывести порядковый номер
+каждого из них (используй метод ordinal()) с новой строки.
+*/
+
+public enum JavarushQuest {
+    JAVA_SYNTAX,
+    JAVA_CORE,
+    JAVA_MULTITHREADING,
+    JAVA_COLLECTIONS,
+    CS_50,
+    ANDROID,
+    GAMES
+}
