2accf93a63f3102daa4c52fe0dd0e8ea52039b71
Alexander Smolin <alexjavacore@mail.ru>
2022-10-26 22:26:12 +0300

    Task0507

---
+++ Tasks05/task1408/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task14.task1408;
+
+/* 
+Проглатываем исключение
+Перехвати исключение в методе main, чтобы программа компилировалась.
+При возникновении исключения нужно вывести на экран строку errorMessage.
+*/
+
+public class Solution {
+
+    public static String errorMessage = "не повезло";
+
+    public static void main(String[] args) {
+        try {
+            generateLuckyNumber();
+        } catch (Exception e) {
+            System.out.println(errorMessage);
+        }
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception();
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
+}
