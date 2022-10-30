60ec2b5fe966ebfe9981d624caa25b5797baf7cd
Alexander Smolin <alexjavacore@mail.ru>
2022-10-30 21:35:49 +0300

    Update Solution.java

--- Tasks06/task0610/Solution.java
+++ Tasks06/task0610/Solution.java
@@ -24,12 +24,8 @@ Path можно получить используя статический ме�
 public class Solution {
     public static void main(String[] args) throws IOException {
         byte[] bytes = args[0].getBytes();
-        try (InputStream stream = System.in;
-             Scanner scanner = new Scanner(stream)){
-             Files.write(Path.of(scanner.nextLine()), bytes);
-        } catch (IOException e) {
-            System.out.println("Something went wrong : " + e);
-        }
+        Scanner scanner = new Scanner(System.in);
+        Files.write(Path.of(scanner.nextLine()), bytes);
     }
 }
 
