23fd81e5c76b3e8933757a992e458a6b3e1ba8f9
Alexander Smolin <alexjavacore@mail.ru>
2022-11-08 09:13:10 +0300

    Task0812

---
+++ Tasks08/task1712/Eatable.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task17.task1712;
+
+/* 
+Съедобное — несъедобное
+Имплементируй интерфейс Eatable только для классов,
+объекты которых могут быть съедобными, из этого списка:
+Apple — яблоко
+Carrot — морковь
+Fire — огонь
+Fish — рыба
+Stone — камень
+*/
+
+public interface Eatable {
+}
