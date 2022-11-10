651c86367d31e0075b0b64eb13858461df057946
Alexander Smolin <alexjavacore@mail.ru>
2022-11-10 11:56:38 +0300

    Task0914

---
+++ Tasks09/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task18.task1814;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+/* 
+Без повторов
+Метод getDistinct(ArrayList<String>) получает список слов,
+в котором содержатся повторы. Твоя задача — реализовать этот метод так,
+чтобы он возвращал поток слов без повторов (каждое слово — по одному разу).
+В этом тебе поможет метод distinct() объекта типа Stream<String>.
+
+Метод main() не принимает участие в тестировании.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var words = new ArrayList<String>();
+        Collections.addAll(words, "чтобы", "стать", "программистом", "нужно", "программировать",
+                "а", "чтобы", "программировать", "нужно", "учиться");
+
+        Stream<String> distinctWords = getDistinctWords(words);
+        distinctWords.forEach(System.out::println);
+    }
+
+    public static Stream<String> getDistinctWords(ArrayList<String> words) {
+        return words.stream().distinct();
+    }
+}
