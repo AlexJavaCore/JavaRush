ec1e0269ad3d2451cc9278339493124742fd20f8
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:18:24 +0300

    Task1122

---
+++ Tasks11/task1122/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task11.task1122;
+
+/* 
+Помощь шахматной школе
+Добавь общий базовый класс ChessPieces к классам фигур (фигуры из шахмат).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class ChessPieces {
+
+    }
+
+    public class King extends ChessPieces {
+    }
+
+    public class Queen extends ChessPieces {
+    }
+
+    public class Rook extends ChessPieces {
+    }
+
+    public class Knight extends ChessPieces {
+    }
+
+    public class Bishop extends ChessPieces {
+    }
+
+    public class Pawn extends ChessPieces {
+    }
+}
