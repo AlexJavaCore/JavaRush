16f4a3c67081309fd4689b720005853551a6df3e
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:57:31 +0300

    Task1206

---
+++ Tasks12/task1206/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+Напиши два метода: print(int) и print(String).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    private static void print(int x) {
+
+    }
+
+    private static void print(String s) {
+
+    }
+}
