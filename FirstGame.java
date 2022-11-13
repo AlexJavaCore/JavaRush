d2a26681f7193f6d54de6e0ab3cb11eaa0ba9bec
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 08:55:43 +0300

    Add files via upload

---
+++ FirstGame.java
@@ -0,0 +1,16 @@
+package com.javarush.games.minigames.mini01;
+
+import com.javarush.engine.cell.Game;
+
+/*
+Чтобы запустить игру, создай класс FirstGame и унаследуй его от класса Game.
+Также переопредели метод "игрового движка" — initialize().
+Попробуй запустить на выполнение класс FirstGame и посмотри, что получилось.
+*/
+
+public class FirstGame extends Game {
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+}
