36fc13e63113476a5a2cf542670d97f76de0fe46
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 21:12:32 +0300

    Task1209

---
+++ Tasks12/task1209/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+Напиши public static методы:
+int min(int, int), long min(long, long), double min(double, double).
+Каждый метод должен возвращать минимальное из двух переданных в него чисел.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public static long min(long a, long b) {
+        return Math.min(a, b);
+    }
+
+    public static double min(double a, double b) {
+        return Math.min(a, b);
+    }
+}
