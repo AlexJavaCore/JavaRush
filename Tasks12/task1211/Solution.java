bf6d5735291b75efe202e7e3a69a049af41ce791
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 21:22:51 +0300

    Task1211

---
+++ Tasks12/task1211/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1211;
+
+/* 
+Абстрактный класс Pet
+Сделай класс Pet абстрактным.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+
+}
