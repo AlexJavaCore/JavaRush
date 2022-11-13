52445e821a594d731f72552c6df8133e0080d97e
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:16:37 +0300

    Task1121

---
+++ Tasks11/task1121/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task11.task1121;
+
+/* 
+Чужой код, незнакомый
+Исправь наследование в классах Cat, Dog, Pet, House, Airplane,
+чтобы программа компилировалась и работала.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
