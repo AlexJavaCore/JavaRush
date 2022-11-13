7f9a857fb0da3115f6b8c2d1394ff974c40caaed
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 17:32:42 +0300

    Task1102

---
+++ Tasks11/task1101/task1102/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task11.task1102;
+
+/* 
+Домашние животные
+Отредактируй три класса: Pet (домашнее животное), Cat (кот) и Dog (собака).
+Унаследуй кота и собаку от животного.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
