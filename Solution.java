3c1eeddcd75b41009b22172766dce0ca3f953639
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 17:30:21 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task11.task1101;
+
+/* 
+Лошадь и Пегас
+Отредактируй два класса — Horse (лошадь) и Pegasus (пегас).
+Унаследуй пегаса от лошади.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse{
+
+    }
+}
