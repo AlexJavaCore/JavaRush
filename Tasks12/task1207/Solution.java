547719b4ad97e7f206e033bbeb4a4c2b8e73b212
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 21:03:25 +0300

    Task1207

---
+++ Tasks12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+Реализуй два метода: print(int) и print(Integer).
+Напиши такой код в методе main(), чтобы вызвались оба.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(5);
+        print((Integer) 10);
+    }
+
+    public static void print(int x) {
+
+    }
+
+    public static void print(Integer x) {
+
+    }
+}
