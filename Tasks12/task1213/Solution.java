4315d99afc55c42ba7540669f38f4c904254db7c
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 21:26:32 +0300

    Task1213

---
+++ Tasks12/task1213/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1213;
+
+/* 
+«Исправь код», часть 2
+Исправь код, чтобы программа компилировалась.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public abstract static class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+
+}
