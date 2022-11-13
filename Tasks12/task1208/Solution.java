63a69f7db058cd393bbd4c3e09426d19a524cb5e
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 21:05:56 +0300

    Task1208

---
+++ Tasks12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+Напиши пять методов print() с разными параметрами.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print() {
+
+    }
+
+    public static void print(int x) {
+
+    }
+
+    public static void print(double d) {
+
+    }
+
+    public static void print(double d, String s) {
+
+    }
+
+    public static void print(float f, String s) {
+
+    }
+}
