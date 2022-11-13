c820f112016f3c3cfab3aafee1c8b5588e7a0515
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:00:53 +0300

    Task1114

---
+++ Tasks11/task1114/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task11.task1114;
+
+/* 
+Происхождение видов
+Измени три класса: Fish (рыба), Lizard (ящерица), Dinosaur (динозавр).
+Унаследуй ящерицу от рыбы и динозавра от ящерицы.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish {
+
+    }
+
+    public class Dinosaur extends Lizard{
+
+    }
+
+}
