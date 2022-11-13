66ba7f3d47963c907138fec6543b61497c416b63
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:02:27 +0300

    Task1115

---
+++ Tasks11/task1115/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task11.task1115;
+
+/* 
+От школьника до пенсии
+Измени четыре класса: Schoolboy (школьник), Student (студент),
+Worker (Сотрудник), Retiree (Пенсионер).
+Унаследовать студента от школьника, сотрудника от студента, пенсионера от сотрудника.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student{
+
+    }
+
+    public class Retiree extends Worker{
+
+    }
+
+}
