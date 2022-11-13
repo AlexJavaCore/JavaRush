37c8ac91c421fbe0ebdfc07e04e25db044f03efc
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 17:35:57 +0300

    Task1103

---
+++ Tasks11/task1103/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task11.task1103;
+
+/* 
+Межпланетная ферма
+Напиши шесть классов: Animal (животное), Cow (корова), Pig (свинья),
+Sheep (овца), Bull (бык) и Goat (козел).
+Унаследуй корову, свинью, овцу, быка и козла от животного.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Sheep extends Animal {
+
+    }
+
+    public class Bull extends Animal {
+
+    }
+
+    public class Goat extends Animal {
+
+    }
+
+}
