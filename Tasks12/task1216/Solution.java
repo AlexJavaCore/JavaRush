b961c15a20fc5b8a2240af59c1197e8d012d7d4c
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 22:09:52 +0300

    Task1216

---
+++ Tasks12/task1216/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+Напиши свой public интерфейс CanFly(летать).
+Добавь в него два метода.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+   public interface CanFly {
+        void fly();
+        void run();
+   }
+}
