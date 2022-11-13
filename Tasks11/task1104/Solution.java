b3201991814cff2c545088551981bbb9195a0e40
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 17:38:10 +0300

    Task1104

---
+++ Tasks11/task1104/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task11.task1104;
+
+/* 
+Хороши работнички
+Напиши четыре класса: Employee (сотрудник), Manager (управляющий),
+Chief (директор) и Secretary (секретарь).
+Унаследуй управляющего, директора и секретаря от сотрудника.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
