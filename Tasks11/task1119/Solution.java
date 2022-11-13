018ae7fc56ec0f41bb6bd09a55f743b7bf72b08d
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:12:56 +0300

    Task1119

---
+++ Tasks11/task1119/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task11.task1119;
+
+/* 
+Четвертая правильная «цепочка наследования»
+Расставь правильно «цепочку наследования» в классах:
+House (дом), Cat (кот), Dog (собака), Car (машина).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
