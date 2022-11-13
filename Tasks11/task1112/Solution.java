fab716aa0702d70bac629af87761841c4fe831fc
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 18:06:29 +0300

    Task1112

---
+++ Tasks11/task1112/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task11.task1112;
+
+/* 
+iPhone и Apple
+Измени два класса — Apple и IPhone.
+Унаследуй IPhone от Apple.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Apple {
+
+    }
+
+    public class IPhone extends Apple {
+
+    }
+}
