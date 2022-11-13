6d8b25df8a2b71b3cdd9486e3946148bcc60733f
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 20:09:14 +0300

    Task1118

---
+++ task1118/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task11.task1118;
+
+/* 
+Городское хозяйство
+Расставь правильно «цепочку наследования» в классах:
+Pet (домашнее животное), Cat (кот), Dog (собака), Car (машина).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
