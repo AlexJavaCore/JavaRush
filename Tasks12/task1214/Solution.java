31f36aeda10f7e7aeeeee5e7f761bebee6fd682c
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 21:28:13 +0300

    Task1214

---
+++ Tasks12/task1214/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1214;
+
+/* 
+Корова — тоже животное
+Унаследуй класс Cow от Animal.
+Реализуй все недостающие методы в классе Cow.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Animal {
+        public abstract String getName();
+    }
+
+    public static class Cow extends Animal {
+        @Override
+        public String getName() {
+            return "Борька";
+        }
+    }
+
+}
