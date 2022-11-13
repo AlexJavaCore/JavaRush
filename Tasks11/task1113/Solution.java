48f52996d3f740ebe7b387aa8634f57584e27e51
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 19:59:23 +0300

    Task1113

---
+++ Tasks11/task1113/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task11.task1113;
+
+/* 
+Играем в Дарвина
+Измени четыре класса: Fish (Рыба), Animal (Животное), Ape (Обезьяна), Human (Человек).
+Унаследовать животное от рыбы, обезьяну — от животного и человека от обезьяны.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
