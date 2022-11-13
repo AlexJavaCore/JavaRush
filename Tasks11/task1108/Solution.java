3355bb65d9e9bbc609a22ba74739b5c38d70c7d0
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 17:51:04 +0300

    Task1108

---
+++ Tasks11/task1108/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task11.task1108;
+
+/* 
+Неприступный кот
+Скрой все внутренние переменные класса Cat,
+а также методы, позволяющие менять внутреннее состояние объектов класса Cat.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        private void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
