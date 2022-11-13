aa3e89d9da7cac0e94e74f7d99eff0ae1d69a294
Alexander Smolin <alexjavacore@mail.ru>
2022-11-13 18:04:46 +0300

    Task11

---
+++ Tasks11/task1111/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task11.task1111;
+
+/* 
+«Из ребра его»
+Измени два класса — Adam (Адам) и Eve (Ева).
+Унаследуй Еву от Адама.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
