97656d6ea72effee1d4db16b57dc0a3b04e9ea3d
Alexander Smolin <alexjavacore@mail.ru>
2022-11-14 10:17:01 +0300

    Task1229

---
+++ Tasks12/task1229/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task12.task1229;
+
+/* 
+Родитель класса CTO
+Добавь такой класс-родитель к классу CTO (технический директор),
+чтобы класс перестал быть абстрактным.
+Добавлять/реализовывать методы в классе CTO запрещается.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static abstract class Parent{
+        public void workHard(){
+
+        }
+    }
+
+    public static class CTO extends Parent implements Businessman {
+
+    }
+}
