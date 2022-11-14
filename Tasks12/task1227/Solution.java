c7eef416531306893dc9f1141103126f60e1dfb2
Alexander Smolin <alexjavacore@mail.ru>
2022-11-14 10:11:45 +0300

    Task1227

---
+++ Tasks12/task1227/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task12.task1227;
+
+/* 
+CanFly, CanRun, CanSwim для классов Duck, Penguin, Toad
+Есть интерфейсы CanFly (летать), CanSwim (плавать), CanRun (бегать).
+Добавь эти интерфейсы классам Duck (утка), Penguin (пингвин), Toad (жаба)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+    public class Duck implements CanFly, CanSwim, CanRun{
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Penguin implements CanRun, CanSwim {
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Toad implements CanSwim{
+
+        @Override
+        public void swim() {
+
+        }
+    }
+}
