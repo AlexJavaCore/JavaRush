a160875ded0d21ea506408281e4f601f8eba83d4
Alexander Smolin <alexjavacore@mail.ru>
2022-11-21 23:53:59 +0300

    Task1411

---
+++ Tasks14/task1411/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Loser, Coder and Proger
+Давай напишем программу, которая определит, чем заняться тому или иному человеку.
+Для этого нужно:
+Ввести [в цикле] с клавиатуры несколько строк (ключей).
+Строки (ключи) могут быть такими: "user", "loser", "coder", "proger".
+Ввод окончен, когда строка не совпадает ни с одной из выше указанных.
+Для каждой введенной строки нужно:
+Создать соответствующий объект [см. Person.java], например,
+для строки "user" нужно создать объект класса User.
+Передать этот объект в метод doWork.
+Написать реализацию метода doWork, который:
+Вызывает метод live() у переданного объекта, если этот объект (person) имеет тип User.
+Вызывает метод doNothing(), если person имеет тип Loser.
+Вызывает метод writeCode(), если person имеет тип Coder.
+Вызывает метод enjoy(), если person имеет тип Proger.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        while (true) {
+            key = reader.readLine();
+            boolean u = key.equals("user") || key.equals("loser") || key.equals("coder") || key.equals("proger");
+            if (!u) break;
+
+            switch (key) {
+                case "user":
+                    person = new Person.User();
+                    break;
+                case "loser":
+                    person = new Person.Loser();
+                    break;
+                case "coder":
+                    person = new Person.Coder();
+                    break;
+                case "proger":
+                    person = new Person.Proger();
+                    break;
+            }
+            doWork(person);
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User) {
+            Person.User user = (Person.User) person;
+            user.live();
+        } else if (person instanceof Person.Loser) {
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
+        } else if (person instanceof Person.Coder) {
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
+        } else if (person instanceof Person.Proger) {
+            Person.Proger proger = (Person.Proger) person;
+            proger.enjoy();
+        }
+    }
+}
