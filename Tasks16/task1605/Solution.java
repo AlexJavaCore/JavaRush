15286397d00e2703d20f9442cf04de3e59600c6a
Alexander Smolin <alexjavacore@mail.ru>
2022-12-23 00:21:35 +0300

    Task1605

---
+++ Tasks16/task1605/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task16.task1605;
+
+import java.util.Date;
+
+/* 
+Поговорим о музыке
+1. Измени класс Violin так, чтоб он стал таском для нити. Используй интерфейс MusicalInstrument
+2. Реализуй необходимый метод в нити Violin. Реализация должна быть следующей:
+2.1. Считай время начала игры - метод startPlaying().
+2.2. Подожди 1 секунду - метод sleepNSeconds(int n), где n - количество секунд.
+2.3. Считай время окончания игры - метод stopPlaying().
+2.4. Выведи на консоль продолжительность игры в миллисекундах. Используй методы из пунктов 2.1 и 2.3.
+
+Пример "Playing 1002 ms".
+*/
+
+public class Solution {
+    public static int delay = 1000;
+
+    public static void main(String[] args) {
+        Thread violin = new Thread(new Violin("Player"));
+        violin.start();
+    }
+
+    public static void sleepNSeconds(int n) {
+        try {
+            Thread.sleep(n * delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public interface MusicalInstrument extends Runnable {
+        Date startPlaying();
+
+        Date stopPlaying();
+    }
+
+    public static class Violin implements MusicalInstrument {
+        private String owner;
+
+        public Violin(String owner) {
+            this.owner = owner;
+        }
+
+        public Date startPlaying() {
+            System.out.println(this.owner + " is starting to play");
+            return new Date();
+        }
+
+        public Date stopPlaying() {
+            System.out.println(this.owner + " is stopping playing");
+            return new Date();
+        }
+
+        @Override
+        public void run() {
+            Date t1 = startPlaying();
+            sleepNSeconds(1);
+            Date t2 = stopPlaying();
+            System.out.println(String.format("Playing %s ms", t2.getTime()-t1.getTime()));
+        }
+    }
+}
