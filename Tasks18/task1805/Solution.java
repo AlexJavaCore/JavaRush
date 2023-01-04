5af59a2db738ddaf3d5db9818f820b78fabeb497
Alexander Smolin <alexjavacore@mail.ru>
2023-01-04 17:10:39 +0300

    Task1805

---
+++ Tasks18/task1805/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task18.task1805;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Сортировка байт
+Ввести с консоли имя файла.
+Считать все байты из файла.
+Не учитывая повторений - отсортировать их по байт-коду в возрастающем порядке.
+Вывести на экран.
+Закрыть поток ввода-вывода.
+
+Пример байт входного файла:
+44 83 44
+
+Пример вывода:
+44 83
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+
+        Set<Integer> bytes = new TreeSet<>();
+        try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+            while (fileInputStream.available() > 0) {
+                bytes.add(fileInputStream.read());
+            }
+        }
+        for (Integer aByte : bytes) {
+            System.out.print(aByte + " ");
+        }
+    }
+}
