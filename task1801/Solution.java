5db8eed6425f1c06016981adf1ac74d2cbe108df
Alexander Smolin <alexjavacore@mail.ru>
2023-01-04 16:52:36 +0300

    Add files via upload

---
+++ task1801/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальный байт
+Ввести с консоли имя файла.
+Найти максимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        int maxByte = 0;
+        try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+            while (fileInputStream.available() > 0) {
+                int currByte = fileInputStream.read();
+                if(currByte > maxByte) maxByte = currByte;
+            }
+        }
+            System.out.println(maxByte);
+    }
+}
