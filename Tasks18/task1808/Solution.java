0d12c99f8c71f124637844158d5047db657a1ce4
Alexander Smolin <alexjavacore@mail.ru>
2023-01-05 23:19:53 +0300

    Task1808

---
+++ Tasks18/task1808/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task18.task1808;
+
+import java.io.*;
+
+/* 
+Разделение файла
+Считать с консоли три имени файла: файл1, файл2, файл3.
+Разделить файл1 по следующему критерию:
+Первую половину байт записать в файл2, вторую половину байт записать в файл3.
+Если в файл1 количество байт нечетное, то файл2 должен содержать большую часть.
+Закрыть потоки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputFile = reader.readLine();
+        String outputFile1 = reader.readLine();
+        String outputFile2 = reader.readLine();
+
+        try (FileInputStream fileInputStream = new FileInputStream(inputFile);
+            FileOutputStream fileOutputStream1 = new FileOutputStream(outputFile1);
+            FileOutputStream fileOutputStream2 = new FileOutputStream(outputFile2)) {
+
+            int halfOfFile = (fileInputStream.available() + 1) / 2;
+            int numberOfByte = 0;
+            while(fileInputStream.available() > 0) {
+                if (numberOfByte < halfOfFile) {
+                    fileOutputStream1.write(fileInputStream.read());
+                    numberOfByte++;
+                } else fileOutputStream2.write(fileInputStream.read());
+            }
+        }
+    }
+}
