74700ac52207f5d59f93b2cbdc9813a03d8c1aa7
Alexander Smolin <alexjavacore@mail.ru>
2023-01-07 11:42:42 +0300

    Task1812

---
+++ Tasks18/task1812/QuestionFileOutputStream.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task18.task1812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Расширяем AmigoOutputStream
+Используя шаблон проектирования Wrapper (Decorator) расширь функциональность AmigoOutputStream.
+В классе QuestionFileOutputStream при вызове метода close()
+должна быть реализована следующая функциональность:
+1. Вывести в консоль фразу "Вы действительно хотите закрыть поток? Д/Н".
+2. Считай строку.
+3. Если считанная строка равна "Д", то закрыть поток.
+4. Если считанная строка не равна "Д", то не закрывать поток.
+*/
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+    private AmigoOutputStream amigoOutputStream;
+    private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public QuestionFileOutputStream(AmigoOutputStream outputStream) {
+        this.amigoOutputStream = outputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        if(bufferedReader.readLine().equals("Д")) amigoOutputStream.close();
+    }
+}
+
