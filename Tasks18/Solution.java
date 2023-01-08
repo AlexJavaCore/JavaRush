d6c198caede846061f741ef3f87920eb3ecb4de7
Alexander Smolin <alexjavacore@mail.ru>
2023-01-08 15:24:15 +0300

    Add files via upload

---
+++ Tasks18/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task18.task1820;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Округление чисел
+Считать с консоли 2 имени файла.
+Первый файл содержит вещественные(дробные) числа, разделенные пробелом. Например, 3.1415.
+Округлить числа до целых и записать через пробел во второй файл.
+Закрыть потоки.
+
+Принцип округления:
+3.49 => 3
+3.50 => 4
+3.51 => 4
+-3.49 => -3
+-3.50 => -3
+-3.51 => -4
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(fileName1));
+             PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2))) {
+
+            while (bufferedFileReader.ready()) {
+                String[] splittedLine = bufferedFileReader.readLine().split(" ");
+                for (String numberInString : splittedLine) {
+                    double number = Double.parseDouble(numberInString);
+                    long roundedNumber = Math.round(number);
+                    printWriter.print(roundedNumber + " ");
+                }
+            }
+        }
+    }
+}
