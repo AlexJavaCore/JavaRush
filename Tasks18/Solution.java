ab0b0960f8e0db9ed5442b202b16364c9772be9d
Alexander Smolin <alexjavacore@mail.ru>
2023-01-08 23:20:18 +0300

    Add files via upload

---
+++ Tasks18/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task18.task1821;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/* 
+Встречаемость символов
+Программа запускается с одним параметром - именем файла, который содержит английский текст.
+Посчитать частоту встречания каждого символа.
+Отсортировать результат по возрастанию кода ASCII (почитать в инете).
+
+Пример:
+','=44, 's'=115, 't'=116.
+
+Вывести на консоль отсортированный результат:
+[символ1] частота1
+[символ2] частота2
+Закрыть потоки.
+
+Пример вывода:
+, 19
+- 7
+f 361
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        int[] aSCII = new int[128];
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                aSCII[reader.read()]++;
+            }
+        }
+        for (int i = 0; i < aSCII.length; i++) {
+            if(aSCII[i] != 0) {
+                System.out.println((char) i + " " + aSCII[i]);
+            }
+        }
+    }
+}
